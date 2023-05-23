2d5d2ac237348c95809a240281c64efe7cbcaec7
Abdallah Hussein <abiza2013@gmail.com>
2023-05-23 19:37:57 +0300

    Adding Report Form

--- TrainBooking/TrainBooking/ReportForm.Designer.cs
+++ TrainBooking/TrainBooking/ReportForm.Designer.cs
@@ -28,57 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ReportForm));
-            this.numberticket = new System.Windows.Forms.TextBox();
-            this.numTicket = new System.Windows.Forms.Label();
-            this.passengernum = new System.Windows.Forms.TextBox();
-            this.adminnum = new System.Windows.Forms.TextBox();
+            this.NCustomer = new System.Windows.Forms.TextBox();
             this.numofpassenger = new System.Windows.Forms.Label();
-            this.numAdmin = new System.Windows.Forms.Label();
-            this.pricestotal = new System.Windows.Forms.TextBox();
+            this.TotalPrice = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.numofcapacity = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.numseats = new System.Windows.Forms.TextBox();
-            this.label = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // numberticket
+            // NCustomer
             // 
-            this.numberticket.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-            this.numberticket.Location = new System.Drawing.Point(304, 126);
-            this.numberticket.Name = "numberticket";
-            this.numberticket.Size = new System.Drawing.Size(265, 34);
-            this.numberticket.TabIndex = 21;
-            // 
-            // numTicket
-            // 
-            this.numTicket.AutoSize = true;
-            this.numTicket.BackColor = System.Drawing.SystemColors.Highlight;
-            this.numTicket.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-            this.numTicket.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.numTicket.Location = new System.Drawing.Point(29, 126);
-            this.numTicket.Name = "numTicket";
-            this.numTicket.Size = new System.Drawing.Size(198, 29);
-            this.numTicket.TabIndex = 20;
-            this.numTicket.Text = "Number of Ticket";
-            // 
-            // passengernum
-            // 
-            this.passengernum.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-            this.passengernum.Location = new System.Drawing.Point(304, 66);
-            this.passengernum.Name = "passengernum";
-            this.passengernum.PasswordChar = '*';
-            this.passengernum.Size = new System.Drawing.Size(265, 34);
-            this.passengernum.TabIndex = 19;
-            // 
-            // adminnum
-            // 
-            this.adminnum.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-            this.adminnum.Location = new System.Drawing.Point(304, 9);
-            this.adminnum.Name = "adminnum";
-            this.adminnum.Size = new System.Drawing.Size(265, 34);
-            this.adminnum.TabIndex = 18;
+            this.NCustomer.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.NCustomer.Location = new System.Drawing.Point(459, 9);
+            this.NCustomer.Name = "NCustomer";
+            this.NCustomer.Size = new System.Drawing.Size(265, 34);
+            this.NCustomer.TabIndex = 19;
             // 
             // numofpassenger
             // 
@@ -86,31 +51,19 @@
             this.numofpassenger.BackColor = System.Drawing.SystemColors.Highlight;
             this.numofpassenger.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
             this.numofpassenger.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.numofpassenger.Location = new System.Drawing.Point(27, 69);
+            this.numofpassenger.Location = new System.Drawing.Point(27, 9);
             this.numofpassenger.Name = "numofpassenger";
-            this.numofpassenger.Size = new System.Drawing.Size(248, 29);
+            this.numofpassenger.Size = new System.Drawing.Size(397, 29);
             this.numofpassenger.TabIndex = 17;
-            this.numofpassenger.Text = "Number of Passenger";
+            this.numofpassenger.Text = "Number of Customers in the System";
             // 
-            // numAdmin
+            // TotalPrice
             // 
-            this.numAdmin.AutoSize = true;
-            this.numAdmin.BackColor = System.Drawing.SystemColors.Highlight;
-            this.numAdmin.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-            this.numAdmin.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.numAdmin.Location = new System.Drawing.Point(27, 9);
-            this.numAdmin.Name = "numAdmin";
-            this.numAdmin.Size = new System.Drawing.Size(200, 29);
-            this.numAdmin.TabIndex = 16;
-            this.numAdmin.Text = "Number of Admin";
-            // 
-            // pricestotal
-            // 
-            this.pricestotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-            this.pricestotal.Location = new System.Drawing.Point(304, 178);
-            this.pricestotal.Name = "pricestotal";
-            this.pricestotal.Size = new System.Drawing.Size(265, 34);
-            this.pricestotal.TabIndex = 23;
+            this.TotalPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.TotalPrice.Location = new System.Drawing.Point(459, 63);
+            this.TotalPrice.Name = "TotalPrice";
+            this.TotalPrice.Size = new System.Drawing.Size(265, 34);
+            this.TotalPrice.TabIndex = 23;
             // 
             // label3
             // 
@@ -118,90 +71,64 @@
             this.label3.BackColor = System.Drawing.SystemColors.Highlight;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
             this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label3.Location = new System.Drawing.Point(27, 183);
+            this.label3.Location = new System.Drawing.Point(29, 63);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(130, 29);
+            this.label3.Size = new System.Drawing.Size(214, 29);
             this.label3.TabIndex = 22;
-            this.label3.Text = "Total Price";
+            this.label3.Text = "Total Price Gained";
             // 
-            // numofcapacity
+            // dataGridView1
             // 
-            this.numofcapacity.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-            this.numofcapacity.Location = new System.Drawing.Point(304, 282);
-            this.numofcapacity.Name = "numofcapacity";
-            this.numofcapacity.Size = new System.Drawing.Size(265, 34);
-            this.numofcapacity.TabIndex = 27;
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 243);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(964, 444);
+            this.dataGridView1.TabIndex = 28;
             // 
-            // label1
+            // label2
             // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.SystemColors.Highlight;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label1.Location = new System.Drawing.Point(27, 287);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(105, 29);
-            this.label1.TabIndex = 26;
-            this.label1.Text = "Capacity";
-            // 
-            // numseats
-            // 
-            this.numseats.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-            this.numseats.Location = new System.Drawing.Point(304, 230);
-            this.numseats.Name = "numseats";
-            this.numseats.Size = new System.Drawing.Size(265, 34);
-            this.numseats.TabIndex = 25;
-            // 
-            // label
-            // 
-            this.label.AutoSize = true;
-            this.label.BackColor = System.Drawing.SystemColors.Highlight;
-            this.label.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-            this.label.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label.Location = new System.Drawing.Point(29, 230);
-            this.label.Name = "label";
-            this.label.Size = new System.Drawing.Size(193, 29);
-            this.label.TabIndex = 24;
-            this.label.Text = "Number of Seats";
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Highlight;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(432, 198);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 29);
+            this.label2.TabIndex = 29;
+            this.label2.Text = "Report";
             // 
             // ReportForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(593, 321);
-            this.Controls.Add(this.numofcapacity);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.numseats);
-            this.Controls.Add(this.label);
-            this.Controls.Add(this.pricestotal);
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(988, 726);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.TotalPrice);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.numberticket);
-            this.Controls.Add(this.numTicket);
-            this.Controls.Add(this.passengernum);
-            this.Controls.Add(this.adminnum);
+            this.Controls.Add(this.NCustomer);
             this.Controls.Add(this.numofpassenger);
-            this.Controls.Add(this.numAdmin);
             this.Name = "ReportForm";
             this.Text = "ReportForm";
+            this.Load += new System.EventHandler(this.ReportForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox numberticket;
-        private System.Windows.Forms.Label numTicket;
-        private System.Windows.Forms.TextBox passengernum;
-        private System.Windows.Forms.TextBox adminnum;
+        private System.Windows.Forms.TextBox NCustomer;
         private System.Windows.Forms.Label numofpassenger;
-        private System.Windows.Forms.Label numAdmin;
-        private System.Windows.Forms.TextBox pricestotal;
+        private System.Windows.Forms.TextBox TotalPrice;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox numofcapacity;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox numseats;
-        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
     }
 }
